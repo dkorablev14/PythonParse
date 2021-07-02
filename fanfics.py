d0ce91b436df40c4528bbad0d76e3d8b0fe62349
Denis <vasjapalkin14@gmail.com>
2021-07-02 15:55:08 +0300

    8

--- fanfics.py
+++ fanfics.py
@@ -4,31 +4,43 @@ import csv
 import re
 
 FILENAME = "fanfics.csv"
-columns = ["name", "mail", "gender", "real_name", "is_author"]
+columns = ["id", "mail", "gender", "real_name", "is_author", ""]
 for i in range(1, 720000):
     user = {}
     url = 'https://fanfics.me/user' + str(i)
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    check_isset = soup.find('tr', {'class': 'wb top_fanf'}).text.strip()
-    if check_isset == 'Запрашиваемый Вами автор не найден':
+    if r.status_code == 404:
         continue
-    centered = soup.find('div', {'class': 'CenteredContent'})
-    tables = centered.find_all('table')
-    user['name'] = tables[0].find('h2').text.strip()
-    data = tables[1].find_all('tr')
-    for tr in data:
-        name = tr.find_all('td')[0].text.strip()
-        value = tr.find_all('td')[1].text.strip()
-        if name == 'E-mail:':
-            user['mail'] = value
-        elif name == 'Пол:':
-            user['gender'] = value
-        elif name == 'Настоящее имя':
-            user['real_name'] = value
+    soup = BeautifulSoup(r.text, 'lxml')
+    profileMain = soup.find('table', {'class': 'ProfileInfo_main'})
+    trs = profileMain.find_all('tr')
+    for tr in trs:
+        tds = tr.find_all('td')
+        if len(tds) > 1:
+            name = tds[0].text.strip()
+            value = tds[1].text.strip()
+            if name == 'Реальное имя:':
+                user['real_name'] = value
+            elif name == 'Пол:':
+                user['gender'] = value
+    profile = soup.find('table', {'class': 'ProfileInfo'})
+    trs = profile.find_all('tr')
+    for tr in trs:
+        tds = tr.find_all('td')
+        if len(tds) > 1:
+            name = tds[0].text.strip()
+            value = tds[1].text.strip()
+            if name == 'Email:':
+                user['mail'] = value
+            elif name == 'Другие сайты:':
+                links = tds[1].find_all('a')
+                # for link in links:
+                #     link = link.get('href')
+                #     link =
+                # user['links']=links
     if re.search('@', user['mail']) is None:
         continue
-    check_author = tables[2].find('h1').text.strip()
+    check_author = soup.find('div', {'class': 'fics_in_profile_container'})
     check_author_reg = re.search('У автора\\s+' + re.escape(user['name']) + '\\s+(\\d+)', check_author).group(1)
     if int(check_author_reg) != 0:
         user['is_author'] = True
