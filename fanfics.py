f33b98bef5aeaf37706f5fd718d5f8d32de4ea17
Denis <vasjapalkin14@gmail.com>
2021-07-05 17:01:02 +0300

    9

--- fanfics.py
+++ fanfics.py
@@ -4,7 +4,7 @@ import csv
 import re
 
 FILENAME = "fanfics.csv"
-columns = ["id", "mail", "gender", "real_name", "is_author", ""]
+columns = ["id", "mail", "gender", "real_name", "is_author", "links"]
 for i in range(1, 720000):
     user = {}
     url = 'https://fanfics.me/user' + str(i)
@@ -24,7 +24,11 @@ for i in range(1, 720000):
             elif name == 'Пол:':
                 user['gender'] = value
     profile = soup.find('table', {'class': 'ProfileInfo'})
+    if profile is None:
+        continue
     trs = profile.find_all('tr')
+    user['links'] = []
+    user['mail'] = ''
     for tr in trs:
         tds = tr.find_all('td')
         if len(tds) > 1:
@@ -34,22 +38,24 @@ for i in range(1, 720000):
                 user['mail'] = value
             elif name == 'Другие сайты:':
                 links = tds[1].find_all('a')
-                # for link in links:
-                #     link = link.get('href')
-                #     link =
-                # user['links']=links
-    if re.search('@', user['mail']) is None:
+                for link in links:
+                    link = link.get('href')
+                    link = link.rpartition('php?url=')
+                    if re.search('vk\\.com|www\\.diary\\.ru|ficbook\\.net|hogwartsnet\\.ru', link[2]) is not None:
+                        user['links'].append(link[2])
+    if user['mail'] == '' or re.search('@', user['mail']) is None:
         continue
     check_author = soup.find('div', {'class': 'fics_in_profile_container'})
-    check_author_reg = re.search('У автора\\s+' + re.escape(user['name']) + '\\s+(\\d+)', check_author).group(1)
+    check_author = check_author.find('div', {'class': 'ProfileLeft_h2_descr'}).text.strip()
+    check_author_reg = re.search('\\d+', check_author).group(0)
     if int(check_author_reg) != 0:
         user['is_author'] = True
     else:
         user['is_author'] = False
     print(i)
     user['id'] = i
-    with open(FILENAME, "a", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=columns)
-        if i == 1:
-            writer.writeheader()
-        writer.writerow(user)
+    # with open(FILENAME, "a", newline="") as file:
+    #     writer = csv.DictWriter(file, fieldnames=columns)
+    #     if i == 1:
+    #         writer.writeheader()
+    #     writer.writerow(user)
