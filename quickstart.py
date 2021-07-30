818dfb665d8d5f7fb281b93baf760a71b83a6b7d
Denis <vasjapalkin14@gmail.com>
2021-07-30 15:55:09 +0300

    12

--- quickstart.py
+++ quickstart.py
@@ -13,64 +13,53 @@ def main():
 
 
 def text():
-    for i in range(1, 486):
-        print(i)
-        url = 'http://fanfics.info/load/?page' + str(i)
-        r = requests.get(url)
-        # time.sleep(5)
+    # https://web.archive.org/web/20190717194856/http://potter-fanfiction.ru:80/
+    # http://nagini-snake.narod.ru/glavnaja.html
+    for i in range(1, 2):
+        # print(i)
+        cookies = {
+            'Cookie': 'onagini-snakeuzll=1626865286; ucvid=4HYBr148X9; _ym_uid=1626865283300121177; _ym_d=1626865283; _cc_session=2cd807a9-7d3a-4ea0-946f-79ac05eacf0c; sspUid=beca3287-2049-4439-ad60-a74f7a8ae463; dmpUid=ywmQT7kj94GqhafPSTXK; _ym_isad=2; onagini-snakedoubt=1; _cc_visit=1; _ym_visorc=b; _cc__visit_deep=12',
+        }
+
+        url = 'http://nagini-snake.narod.ru/index/fanfiki_2006/0-28'
+        r = requests.get(url, cookies=cookies)
         if r.status_code == 404:
             continue
         soup = BeautifulSoup(r.text, 'lxml')
-        entries = soup.find_all("div", id=re.compile('entryID\\d+'))
-        for entry in entries:
-            title = entry.find_all('table')
-            url_card = title[1].find('a')
-            url_card = 'https://fanfic.ucoz.com' + url_card.get('href')
+        tables = soup.find_all("ul", {'class': 'uMenuRoot'})
+        years_all = tables[0].find_all('li')
+        years_fun = []
+        for years in years_all:
+            if re.search('Фанфики', years.text.strip()) is not None:
+                years = years.find('a').get('href')
+                years_fun.append(years)
+            else:
+                continue
+        for year in years_fun:
+            url_card = 'http://nagini-snake.narod.ru' + year
+            r_card = requests.get(url_card, cookies=cookies)
+            soup_card = BeautifulSoup(r_card.text, 'lxml')
+            content = soup_card.find_all('td', {'valign': 'top'})[1]
+            hrefs_no = content.find_all('a')
+            hrefs_yes = []
+            for href in hrefs_no:
+                if re.search('\/index\/', href):
+                    hrefs_yes.append(href)
+            for href in hrefs_yes:
+                r_card = requests.get(url_card, cookies=cookies)
+                soup_card = BeautifulSoup(r_card.text, 'lxml')
+                content = soup_card.find_all('td', {'valign': 'top'})[1]
             file = url_card.split('load/')
-            FILENAME = 'C:/Users/Denis/PycharmProjects/FunFuck2/htmls/' + file[1] + ".html"
+            file = file[1].split('/')
+            FILENAME = '/home/aleks/PycharmProjects/funfuck/htmls/' + file[-1] + ".html"
             r_card = requests.get(url_card)
             # time.sleep(2)
             if r_card.status_code == 404:
                 continue
-            soup_card = BeautifulSoup(r_card.text, 'lxml')
-            content = soup_card.find('div', {'class': 'content'})
+
             with open(FILENAME, "w", encoding='utf-8') as file:
                 file.write(str(content))
-        # profile = soup.find('table', {'class': 'ProfileInfo'})
-        # if profile is None:
-        #     continue
-        # trs = profile.find_all('tr')
-        # user['links'] = []
-        # user['mail'] = ''
-        # for tr in trs:
-        #     tds = tr.find_all('td')
-        #     if len(tds) > 1:
-        #         name = tds[0].text.strip()
-        #         value = tds[1].text.strip()
-        #         if name == 'Email:':
-        #             user['mail'] = value
-        #         elif name == 'Другие сайты:':
-        #             links = tds[1].find_all('a')
-        #             for link in links:
-        #                 link = link.get('href')
-        #                 link = link.rpartition('php?url=')
-        #                 if re.search('vk\\.com|www\\.diary\\.ru|ficbook\\.net|hogwartsnet\\.ru', link[2]) is not None:
-        #                     user['links'].append(link[2])
-        # if user['mail'] == '' or re.search('@', user['mail']) is None:
-        #     continue
-        # if len(user['links']) > 0:
-        # user['links'] = ','.join(user['links'])
-        # check_author = soup.find('div', {'class': 'fics_in_profile_container'})
-        # if check_author is None:
-        #     check_author_reg = 0
-        # else:
-        #     check_author = check_author.find('div', {'class': 'ProfileLeft_h2_descr'}).text.strip()
-        #     check_author_reg = re.search('\\d+', check_author).group(0)
-        # if int(check_author_reg) != 0:
-        #     user['is_author'] = True
-        # else:
-        #     user['is_author'] = False
-        # user['id'] = i
+    # disk()
 
 
 if __name__ == '__main__':
